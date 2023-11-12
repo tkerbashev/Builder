f6cb6b547f219a3acec8700bb625b562fdf7eff6
tkerbashev <tkerbashev@gmail.com>
2023-11-12 16:49:42 +0800

    Add project files.

---
+++ Builder/Program.cs
@@ -0,0 +1,14 @@
+﻿using Builder;
+
+Console.WriteLine( "Building a Bubble Tea" );
+Console.WriteLine( );
+
+var jacksPurchase = new BubbleTeaPurchase(new RegularBlackMangoHalfIceOrder(), "Jack");
+jacksPurchase.MakeTea();
+jacksPurchase.DescribeTea();
+
+Console.WriteLine();
+
+var jillsPurchase = new BubbleTeaPurchase( new BigGreenStrawberryFullIceOrder( ), "Jill" );
+jillsPurchase.MakeTea( );
+jillsPurchase.DescribeTea( );
